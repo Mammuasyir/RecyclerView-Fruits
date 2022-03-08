97abf3048249a68e894aa12bb1defa349011a014
Mammuasyir <mammuasyir0101@gmailcom>
2022-03-08 20:55:35 +0700

    commit

---
+++ app/src/main/java/com/muasyir/recyclerviewfruit/BuahAdapter.kt
@@ -0,0 +1,47 @@
+package com.muasyir.recyclerviewfruit
+
+import android.media.MediaPlayer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+class BuahAdapter(
+    val namaBuah: Array<String>,
+    val gambarBuah: Array<Int>,
+    val suaraBuah: Array<Int>
+) : Adapter<BuahAdapter.MyViewHolder>() {
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvBuah = view.findViewById<TextView>(R.id.item_tv_buah)
+        val imgBuah = view.findViewById<ImageView>(R.id.item_img)
+    }
+
+    // 1. call layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_buah, parent, false)
+        return MyViewHolder(view)
+    }
+
+    //2. mengatur posisi antar data
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.tvBuah.text = namaBuah[position]
+        holder.imgBuah.setImageResource(gambarBuah[position])
+        holder.itemView.setOnClickListener {
+            val mediaPlayer = MediaPlayer.create(holder.itemView.context, suaraBuah[position])
+            mediaPlayer.start()
+        }
+    }
+
+    //3. Count Data /Menampoilkan Data sebanyak data yang ad
+    override fun getItemCount(): Int {
+
+        return namaBuah.size
+    }
+
+}
---
+++ app/src/main/java/com/muasyir/recyclerviewfruit/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.muasyir.recyclerviewfruit
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val namaBuah =
+            arrayOf("Alpukat", "Apel", "Ceri", "Durian", "Jambu Air", "Manggis", "Strawberry")
+
+        val gambarBuah = arrayOf(
+            R.drawable.alpukat,
+            R.drawable.apel,
+            R.drawable.ceri,
+            R.drawable.durian,
+            R.drawable.jambuair,
+            R.drawable.manggis,
+            R.drawable.strawberry
+        )
+        val suaraBuah = arrayOf(
+            R.raw.alpukat,
+            R.raw.apel,
+            R.raw.ceri,
+            R.raw.durian,
+            R.raw.jambu_air,
+            R.raw.manggis,
+            R.raw.strawberry
+        )
+        val buahAdapter = BuahAdapter(namaBuah, gambarBuah, suaraBuah)
+        rv_buah.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+//yang ini biar layouot ny kebagi 2
+//            layoutManager = GridLayoutManager(this@MainActivity, 2)
+            setHasFixedSize(true)
+            adapter = buahAdapter
+        }
+    }
+
+    //    call Layout Menu Buat opsi grid atau list
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_grid) {
+            rv_buah.layoutManager = GridLayoutManager(this@MainActivity, 2)
+        }
+        if (item.itemId == R.id.menu_list) {
+            rv_buah.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
